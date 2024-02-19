95a1c1443a1c7f8d207909b5e304f41a6bca2980
Phasit Ruangmak <phasit2005@gmail.com>
2024-02-19 23:09:55 +0700

    .

--- keypad.py
+++ keypad.py
@@ -4,30 +4,56 @@ from tkinter import ttk
 
 class Keypad(tk.Frame):
     def __init__(self, parent, keynames=[], columns=1, **kwargs):
+        # call the superclass constructor with all args except
+        # keynames and columns
         super().__init__(parent, **kwargs)
         self.keynames = keynames
         self.init_components(columns)
 
     def init_components(self, columns) -> None:
+        """Create a keypad of keys using the keynames list.
+        The first keyname is at the top left of the keypad and
+        fills the available columns left-to-right, adding as many
+        rows as needed.
+        :param columns: number of columns to use
+        """
         for i, keyname in enumerate(self.keynames):
             row, col = divmod(i, columns)
             button = tk.Button(self, text=keyname)
             button.grid(row=row, column=col, sticky="nsew")
             button.bind("<Button>", self.handle_press)
 
-
     def bind(self, sequence=None, func=None, add=None):
+        """Bind an event handler to an event sequence."""
+        # Write a bind method with exactly the same parameters
+        # as the bind method of Tkinter widgets.
+        # Use the parameters to bind all the buttons in the keypad
+        # to the same event handler.
         for child in self.winfo_children():
             child.bind(sequence, func, add)
 
     def __setitem__(self, key, value) -> None:
+        """Overrides __setitem__ to allow configuration of all buttons
+               using dictionary syntax.
+               Example: keypad['foreground'] = 'red'
+               sets the font color on all buttons to red.
+               """
         for child in self.winfo_children():
             child[key] = value
 
     def __getitem__(self, key):
+        """Overrides __getitem__ to allow reading of configuration values
+                from buttons.
+                Example: keypad['foreground'] would return 'red' if the button
+                foreground color is 'red'.
+                """
         return self.winfo_children()[0][key]
 
     def configure(self, cnf=None, **kwargs):
+        """Apply configuration settings to all buttons.
+               To configure properties of the frame that contains the buttons,
+               use `keypad.frame.configure()`.
+               """
         for child in self.winfo_children():
             child.configure(cnf, **kwargs)
 
